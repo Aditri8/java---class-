80ce4ec10838e3bff21709cca6cd02f878bf690f
debjyotiC <djbabai.debjyoti@gmail.com>
2020-07-18 12:05:10 +0530

    Example Java code v1.1

--- Example.java
+++ Example.java
@@ -20,9 +20,6 @@ class TestProject {
     public static void main(String args[]){
         Box mybox_0 = new Box(10.0, 10.0, 10.0);
         System.out.println("Volume: "+mybox_0.volume());
-
-        Box mybox_1 = new Box(20.0, 20.0, 20.0);
-        System.out.println("Volume: "+mybox_1.volume());
     }
 }
 
