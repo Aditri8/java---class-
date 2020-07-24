38fc0f6f1585856814f514cf474ef8310425e963
debjyotiC <djbabai.debjyoti@gmail.com>
2020-07-24 10:48:52 +0530

    Statck class added

--- StackClass.java
+++ StackClass.java
@@ -1,6 +1,34 @@
+class Stack{
+    int stk[] = new int[10];
+    int tos;
+    Stack(){
+        tos = -1;
+    }
+    void push(int item){
+        if(tos==9)
+           System.out.println("Stack overflow");
+        else stk[++tos] = item;
+    }
+    int pop() {
+        if (tos < -1) {
+            System.out.println("Stack underflow");
+            return 0;
+        }
+        else{
+            return stk[tos--];
+        }
+    }
+}
+
 public class StackClass {
     public static void main(String args[]){
-        System.out.println("hello world!");
+        Stack myStack = new Stack();
+        // push it
+        for(int i=0; i<10; i++)
+            myStack.push(i);
+        // pop it
+        for (int j=0; j<10; j++){
+            System.out.println("The "+j+"th element is:"+myStack.pop());
+        }
     }
-
 }
